5de3d0daed15987bc822c361b241fb69d20f7d32
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-06-25 09:09:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,134 +5,59 @@ ferrari = -1001274887387
 
 @app.on_message(Filters.chat(ferrari) & ~ Filters.edited)
 def main(client, message):
- file = open("ferrari.txt" , "r")
- lines = file.readlines()
- file.close()
- for line in lines:
-  p = line.split()
-  for r in p: 
-    mes = client.send_message( int(r), "**" + message.text + "**" )
-    fille = open(str(r)+".txt","r")
-    n = fille.readlines()
-    fille.close()
-    for t in n:
-     fie = open(str(r)+".txt","w")
-     fie.write(t +" " + str(message.message_id) + " " + str(mes.message_id))
-     fie.close()
-
+ mes = client.send_message( int(k), "**" + message.text + "**" )
+ fille = open("ids.txt","r")
+ n = fille.readlines()
+ fille.close()
+ for t in n:
+  fie = open("ids.txt","w")
+  fie.write(t +" " + str(message.message_id) + " " + str(mes.message_id))
+  fie.close()
 
 @app.on_message(Filters.chat(bullet) & ~ Filters.edited)
 def main(client, message):
- file = open("bullet.txt" , "r")
- lines = file.readlines()
- file.close()
- for line in lines:
-  p = line.split()
-  for s in p:
-    mes = client.send_message( int(s), "**" + message.text + "**" )
-    fille = open(str(s)+".txt","r")
-    n = fille.readlines()
-    fille.close()
-    for t in n:
-     fie = open(str(s)+".txt","w")
-     fie.write(t +" " + str(message.message_id) + " " + str(mes.message_id))
-     fie.close()
-
- 
-
+ mes = client.send_message( int(k), "**" + message.text + "**" )
+ fille = open("ids.txt","r")
+ n = fille.readlines()
+ fille.close()
+  for t in n:
+   fie = open("ids.txt","w")
+   fie.write(t +" " + str(message.message_id) + " " + str(mes.message_id))
+   fie.close()
 
 @app.on_message(Filters.chat(ferrari) & Filters.edited)
 def main(client, message):
- file = open("ferrari.txt" , "r")
- lines = file.readlines()
- file.close()
- for line in lines:
-  t = line.split()
-  for m in t:
-   files = open(str(m)+".txt" , "r")
+   files = open("ids.txt" , "r")
    d = files.readlines()
    files.close()
    for c in d:
     x = c.split()
     id = str(message.message_id)
     if id in x:
-      client.edit_message_text(int(m),int(x[x.index(id)+1]), "**" + message.text + "**" )
+      client.edit_message_text(int(k),int(x[x.index(id)+1]), "**" + message.text + "**" )
 
      
 @app.on_message(Filters.chat(bullet) & Filters.edited)
 def main(client, message):
- file = open("bullet.txt" , "r")
- lines = file.readlines()
- file.close()
- for line in lines:
-  b = line.split()
-  for a in b:
-   files = open(str(a)+".txt" , "r")
-   d = files.readlines()
-   files.close()
-   for c in d:
-    x = c.split()
-    id = str(message.message_id)
-    if id in x:
-      client.edit_message_text(int(a),int(x[x.index(id)+1]), "**" + message.text + "**" )
+ files = open("ids.txt" , "r")
+ d = files.readlines()
+ files.close()
+ for c in d:
+  x = c.split()
+  id = str(message.message_id)
+  if id in x:
+   client.edit_message_text(int(k),int(x[x.index(id)+1]), "**" + message.text + "**" )
      
-@app.on_message(Filters.command('add') & Filters.user(491634139) )
-def forward(client, message):
- if len(message.text.split(' ')) > 2:
-  if len(message.text.split(' ')[1]) == 14:
-   with open(message.text.split(" ")[2] + ".txt" , "r") as file:
-    lines = file.readlines()
-    file.close()
-    for line in lines:
-     files = open(message.text.split(" ")[2] + ".txt" , "w") 
-     files.write(line + " " + message.text.split(' ')[1])
-     files.close()
-     with open(message.text.split(' ')[1]+".txt" , "w") as g:
-       g.write("001 002")
-       g.close()
-       message.reply("💾 Done, The chat_id  ```" + message.text.split(' ')[1] +"```🌐 has been added to my database. ✅✅")
-  else:
-    message.reply("💼 Please write a valid chat id. ✅✅ ")
- else:
-   message.reply("💼 Please write a valid chat id. ✅✅ ")
-@app.on_message(Filters.command('remove') & Filters.user(491634139))
-def forward(client, message):
- if len(message.text.split(' ')) > 2:
-  if len(message.text.split(' ')[1]) == 14:
-   file = open(message.text.split(" ")[2] + ".txt" , "r")
-   u = file.readlines()
-   file.close()
-   for v in u:
-     lines = v.split() 
-     del lines[lines.index(message.text.split(' ')[1])]
-     y = " ".join(str(x) for x in lines)
-     files = open(message.text.split(" ")[2] + ".txt" , "w") 
-     files.write(y)
-     files.close()
-     message.reply("💾 Done, The chat_id  ```" + message.text.split(' ')[1] +"```🌐 has been removed to my database. ✅✅")
-  else:
-    message.reply("💼 Please write a valid chat id. ✅✅ ")
- else:
-    message.reply("💼 Please write a valid chat id. ✅✅ ")
+
+
 @app.on_message(Filters.command('clear') & Filters.user(491634139))
 def forward(client, message):
-    file = open(message.text.split(" ")[1] + ".txt" , "r")
-    lines = file.readlines()
-    file.close()
-    for line in lines:
-     p = line.split()
-     for x in p:
-        fie = open(str(x)+".txt","w")
-        fie.write("001 002")
-        fie.close()
-        message.reply("☢️ Done, Editing data cleared ✅✅")
-@app.on_message(Filters.command('list') & Filters.user(491634139))
-def forward(client, message):
-  file = open(message.text.split(" ")[1] + ".txt" , "r")
-  u = file.readlines()
-  file.close()
-  for v in u :
-      message.reply("🏘️ List of Chat_ids in my database are ```" + str(v) + "```. Its can be change. ✅✅")
+      fie = open("ids.txt","w")
+      fie.write("001 002")
+      fie.close()
+      message.reply("☢️ Done, Editing data cleared ✅✅")
+
+
 @app.on_message(Filters.command('get') & Filters.user(491634139) )
 def forward(client, message):
  if len(message.text.split(' ')) > 1:
@@ -143,14 +68,17 @@ def forward(client, message):
     message.reply("💼 Please write a valid chat id. ✅✅ ")
  else:
     message.reply("💼 Please write a valid chat id. ✅✅ ")
+
 @app.on_message(Filters.command("start"))
 def forward(client, message):
  if message.from_user.id == 491634139:
    message.reply("♻️ Welcome to your LineBot . ✅✅")
  else:
    message.reply("♻️ You need admins permission to use my functions. ✅✅")
+
 @app.on_message(Filters.private)
 def forward(client, message):
  if not message.from_user.id == 491634139:
    message.reply("♻️ You need admins permission to use my functions. ✅✅")
+
 app.run()
