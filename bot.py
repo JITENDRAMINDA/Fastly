cfa48645f77de2c8eaf569f6ba1b35253bfa9ab8
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-06-29 11:05:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, Filters
-app = Client("766063335:AAH5z2AYzAwXDlFJCzfFrmUlvRR7FwI84nc",814511,"44462f0f278503255d5cc30941b617a9")
+app = Client("886312685:AAF9H5OKzveB8RrcdhvpuMZFISz8vBK0ESQ",814511,"44462f0f278503255d5cc30941b617a9")
 bullet = -1001378725482
 ferrari = -1001274887387 
-k = -1001356351939
+k = -1001239795477
 
 @app.on_message(Filters.chat(bullet) & ~ Filters.edited)
 def main(client, message):
