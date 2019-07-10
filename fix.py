428f2cdcb339f3268e1a4e4b00c5b3dc951aa79f
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-07-10 18:43:01 +0530

    Create fix.py

---
+++ fix.py
@@ -0,0 +1,10 @@
+from pyrogram import Client, Filters
+app = Client("ssssn",bot_token="859744901:AAEm_YyLvVTkj3LrecJtnmZJ81BibIaBC3E",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
+bullet = -1001378725482                                              
+
+@app.on_message(Filters.chat(bullet))
+def main(client, message):
+  client.edit_message_text(-1001368865846,6080, "**" + message.text + "**" )
+
+
+app.run()
