9fb404a26d73169701cdf1ad4cdae8a551e526ed
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-07-10 14:10:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, Filters
-app = Client("ssssn",bot_token="816611430:AAHGbDn_LNEbERX_aaIBDXtbqEne0t0PsLU",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
+app = Client("ssssn",bot_token="835586107:AAEZUniK1-X_rBdM1v5ZvATbCtnlGyqIBKc",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
 bullet = -1001378725482                                              
 ferrari = -1001274887387                                             
-k = -1001383708894
+k = -1001434376151
 @app.on_message(Filters.chat(bullet) & ~ Filters.edited)
 def main(client, message):
  mes = client.send_message( k, "**" + message.text + "**" )
