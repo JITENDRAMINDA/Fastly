b2c3735ef65c61e2fd7f71662b27627085198e11
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-07-21 09:52:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 from pyrogram import Client, Filters
 import time
 
-app = Client("hii",bot_token="823129829:AAHsKIeP8Nq2kIp3rpdyavNRIpFUnSFx0Hs",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")
+app = Client("hiiw",bot_token="889364243:AAEwu7P8g7MBjp1_ox0taiZp5YEOOCE8slE",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")
 
 #("ssssn",bot_token="835586107:AAEZUniK1-X_rBdM1v5ZvATbCtnlGyqIBKc",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
 bullet = -1001378725482                                              
 ferrari = -1001274887387                                             
-k = -1001404750744
+k = -1001260341021
 
 @app.on_message(Filters.chat(ferrari) & ~ Filters.edited)
 def main(client, message):
